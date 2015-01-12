07516a858b64932c889cf2d413a3557030ff7d04
Lawrence Bogdan Aleksandrov <malwareinform@gmail.com>
2015-01-13 06:29:34 +0800

    release version nigs 0.1

---
+++ grobert.js
@@ -0,0 +1,5 @@
+/*
+	grobert.js copysrighte falyezro twentifeftin
+*/
+
+function grobert(){document.write('grobert');}function grobertCap(){document.write('Grobert');}function grobertVer(){document.write('grobert.js version nigs');}
