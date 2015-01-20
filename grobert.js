f01b1d0b54c2ee5e90e0d371df8884b7a023a507
Lawrence Bogdan Aleksandrov <malwareinform@gmail.com>
2015-01-20 08:40:24 +0800

    release version nigging

--- grobert.js
+++ grobert.js
@@ -2,4 +2,5 @@
 	grobert.js copysrighte falyezro twentifeftin
 */
 
-function grobert(){document.write('grobert');}function grobertCap(){document.write('Grobert');}function grobertVer(){document.write('grobert.js version nigs');}
+function grobert(){$('body').append('grobert');}function Grobert(){$('body').append('Grobert');}function grobertVer(){$('body').append('<div><font color="#ff0000">g</font><font color="#ff1900">r</font><font color="#ff3300">o</font><font color="#ff4c00">b</font><font color="#ff6600">e</font><font color="#ff7f00">r</font><font color="#ff9900">t</font><font color="#ffb200">.</font><font color="#ffcc00">j</font><font color="#ffe500">s</font><font color="#ffff00"> </font><font color="#d5ff00">v</font><font color="#aaff00">e</font><font color="#80ff00">r</font><font color="#55ff00">s</font><font color="#2bff00">i</font><font color="#00ff00">o</font><font color="#00ff33">n</font><font color="#00ff66"> </font><font color="#00ff99">n</font><font color="#00ffcc">i</font><font color="#00ffff">g</font><font color="#00ccff">g</font><font color="#0099ff">i</font><font color="#0066ff">n</font><font color="#0033ff">g</font><font color="#0000ff"> </font></div>');}
+
